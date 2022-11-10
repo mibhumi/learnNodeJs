bd7797ecf5bb7f76605038bcabd951b9f4d10b7d
Bhumi Patel <bhumipatel1196@gmail.com>
2022-11-09 22:21:29 -0500

    display and create blogs using mongoose

--- app.js
+++ app.js
@@ -1,15 +1,57 @@
 const express = require('express');
 const morgan = require('morgan');
-// const ejs = require('ejs');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
 // calling express
 const app = express();
 
+// connect to database
+// listen for request port 3000 if connection to db is successful
+const dbURI = "mongodb+srv://bhumi:12345@learnnodejs.hhjfbi0.mongodb.net/demo?retryWrites=true&w=majority";
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result)=>app.listen(3000))
+    .catch((err)=> console.log('error: ', err));
+
 // register view engine
 app.set('view engine', 'ejs');
 
-// listen for request port 3000
-app.listen(3000);
+// mongoose and mongo sandbox routes
+// app.get('/add-blog', (req,res) => {
+//     const blog = new Blog({
+//         title: 'my new blog',
+//         snippet: 'new snippet',
+//         body: 'my new blog body'
+//     });
+
+//     blog.save()
+//         .then((result)=>{
+//             res.send(result)
+//         })
+//         .catch((err)=>{
+//             console.log(err)
+//         })
+// })
+
+// app.get('/all-blogs', (req,res) => {
+//     Blog.find()
+//         .then((result)=>{
+//             res.send(result);
+//         })
+//         .catch((err)=> { 
+//             console.log(err)
+//         })
+// })
+
+// app.get('/single-blog', (req, res) => {
+//     Blog.findById('636b3dfca2da2fcbf00907b7')
+//         .then((result)=>{
+//             res.send(result)
+//         })
+//         .catch((err)=> { 
+//             console.log(err)
+//         })
+// })
 
 // middleware and static files 
 // app.use(express.static('public')); // inside public folder all the files are accessable by browser
@@ -28,16 +70,30 @@ app.use(morgan('tiny'));
 
 // request
 app.get('/', (req, res) => {
-    const blogs = [
-        {title: 'new york', snippet: 'it is a city'},
-        {title: 'delhi', snippet: 'it is a captial of india'},
-        {title: 'miltion', snippet: 'it is part of ontario'}
-    ];
-    res.render('index', {title: 'Home', blogs});
+
+    res.redirect('/blogs');
+
+    // const blogs = [
+    //     {title: 'new york', snippet: 'it is a city'},
+    //     {title: 'delhi', snippet: 'it is a captial of india'},
+    //     {title: 'miltion', snippet: 'it is part of ontario'}
+    // ];
+    // res.render('index', {title: 'Home', blogs});
 });
 
 app.get('/about', (req, res) => {
     res.render('about', {title: 'About'});
+});
+
+// blog routes
+app.get('/blogs', (req, res) => {
+        Blog.find().sort({createdAt: -1})
+            .then((result)=>{
+                res.render('index', {title: 'All Blogs', blogs: result});
+            })
+            .catch((err)=> { 
+                console.log(err)
+            })
 });
 
 app.get('/blog/create', (req, res) => {
